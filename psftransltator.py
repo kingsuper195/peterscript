694da5ee650c959e846f0113c0058fab81b68a35
King Super <peter.durdin.superworld@gmail.com>
2022-10-02 16:56:01 +1100

    My own coding programming language, test version

---
+++ psftransltator.py
@@ -0,0 +1,54 @@
+
+from logging import error, log
+
+
+def loadProgram(filename):
+    f = open(filename)
+    lines = f.readlines()
+    return lines
+
+lines = loadProgram("test.pst")
+
+def do_write(line):
+    output = line.removeprefix("(write-").removesuffix(")")
+    output = output.replace('-'," ")
+    print(output)
+    #print("alert('{output}');".format(output=output))
+
+def do_repeat(line):
+    line = line.removeprefix("|Repeat-").removesuffix("|")
+    parts = line.split("(")
+    count = int(parts.pop(0))
+    #print("for(var i=0; i<{count};i+++){{".format(count=count))
+    for x in range(count):
+        for part in parts:
+            command = "(" + part
+            do_command(command)
+    #print("}")
+
+def do_command(line):
+    # Test if the line is a (write-message) 
+    if (line.startswith("(write-") and line.endswith(")")):
+        do_write(line)
+        return
+    # |Repeat-<count>(<command>)|
+    if(line.startswith("|Repeat-") and line.endswith("|")):
+        do_repeat(line)
+        return
+
+    print("err: '{line}'".format(line=line))
+
+
+for line in lines:
+    line = line.strip()
+
+    # Test if the line is a blank line
+    if (line == ""):
+        continue
+
+    # Test if the line is a comment
+    if (line.startswith("#")):
+        continue
+
+    # Do all the other commands
+    do_command(line)
